f65042d9954c08fa69a93e3aac87bc2a9f4ee435
Andrew <acvdoandrew@gmail.com>
2024-10-24 23:31:25 -0400

    adds constants

---
+++ lib/constants/draftPhases.ts
@@ -0,0 +1,24 @@
+import { DraftPhase } from "@/types/draft";
+
+export const draftPhases: DraftPhase[] = [
+    { team: 'Blue', action: 'Ban', index: 0 },
+    { team: 'Red', action: 'Ban', index: 0 },
+    { team: 'Blue', action: 'Ban', index: 1 },
+    { team: 'Red', action: 'Ban', index: 1 },
+    { team: 'Blue', action: 'Ban', index: 2 },
+    { team: 'Red', action: 'Ban', index: 2 },
+    { team: 'Blue', action: 'Pick', index: 0 },
+    { team: 'Red', action: 'Pick', index: 0 },
+    { team: 'Red', action: 'Pick', index: 1 },
+    { team: 'Blue', action: 'Pick', index: 1 },
+    { team: 'Blue', action: 'Pick', index: 2 },
+    { team: 'Red', action: 'Pick', index: 2 },
+    { team: 'Red', action: 'Ban', index: 3 },
+    { team: 'Blue', action: 'Ban', index: 3 },
+    { team: 'Red', action: 'Ban', index: 4 },
+    { team: 'Blue', action: 'Ban', index: 4 },
+    { team: 'Red', action: 'Ban', index: 3 },
+    { team: 'Blue', action: 'Ban', index: 3 },
+    { team: 'Blue', action: 'Ban', index: 4 },
+    { team: 'Red', action: 'Ban', index: 4 },
+];
