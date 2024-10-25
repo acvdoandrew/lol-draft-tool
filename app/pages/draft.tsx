d76b58c5c543dd0391f0eadd5bc81b04f290090b
Andrew <acvdoandrew@gmail.com>
2024-10-24 22:47:08 -0400

    removes old code

--- app/pages/draft.tsx
+++
@@ -1,5 +0,0 @@
-import { GetStaticProps } from 'next';
-import { useContext } from 'react';
-import { fetchChampions } from '@/utils/fetchChampions';
-import { ChampionsData } from '@/utils/types';
-import { DraftContext } from '../contexts/DraftContext';
